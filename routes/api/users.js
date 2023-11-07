049d9ff2a3a8c95baf2c9c64b52ae93cab83de4c
bhoodev24 <52924917+bhoodev24@users.noreply.github.com>
2023-11-07 13:17:51 +0100

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,9 +10,9 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
-const passport = require("passport");
+// const passport = require("passport");
 //Load Input Validation
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
@@ -829,16 +829,16 @@ router.post("/login", (req, res) => {
 //@route GET api/users/current
 //@desc Return current user
 //@access Private
-router.get(
-  "/current",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.json({
-      id: req.user.id,
-      name: req.user.name,
-      email: req.user.email,
-    });
-  }
-);
+// router.get(
+//   "/current",
+//   passport.authenticate("jwt", { session: false }),
+//   (req, res) => {
+//     res.json({
+//       id: req.user.id,
+//       name: req.user.name,
+//       email: req.user.email,
+//     });
+//   }
+// );
 
 module.exports = router;
